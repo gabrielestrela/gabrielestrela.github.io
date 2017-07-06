8f5772889188059265fa347b7f398dd1cd4146e8
gabrielestrela <gabrielestrela92@gmail.com>
2017-07-06 01:03:00 +0000

    Fixed parallax issues

--- JS/parallax.js
+++ JS/parallax.js
@@ -1,3 +1,19 @@
  $(document).ready(function () {
      $('.parallax').parallax();
+
+
+
+//     if (matchMedia) {
+//         var smallDevice = window.matchMedia("(max-width: 600px)");
+//         smallDevice.addListener(WidthChange);
+//         WidthChange(smallDevice);
+//     }
+
  });
+
+// function WidthChange(smallDevice) {
+//     if (smallDevice.matches) {
+//         $(".parallax img").hide();
+//         $("div").removeClass("parallax parallax-container");
+//     }
+// }
